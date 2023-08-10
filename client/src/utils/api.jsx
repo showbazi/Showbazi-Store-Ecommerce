8a7aa17939cb2906345839567a5951bf17d60c1a
showbazi <mail.bsagar@gmail.com>
2023-08-10 22:56:01 +0530

    fix: baseurl

--- client/src/utils/api.jsx
+++ client/src/utils/api.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an Axios instance with base URL
 const api = axios.create({
-  baseURL: 'http://localhost:8000/',
+  baseURL: 'https://showbazi-store-api.onrender.com/',
 });
 
 // Request Interceptor
