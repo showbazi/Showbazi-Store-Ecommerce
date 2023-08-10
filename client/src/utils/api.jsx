bcd9f5721732ec50f174308309b435bda5ad59bb
showbazi <mail.bsagar@gmail.com>
2023-08-10 21:28:59 +0530

    fix: remove interceptor to check token cookie

--- client/src/utils/api.jsx
+++ client/src/utils/api.jsx
@@ -6,22 +6,22 @@ const api = axios.create({
 });
 
 // Request Interceptor
-api.interceptors.request.use((config) => {
-  // Set headers or perform other request modifications
-  config.headers['Content-Type'] = 'application/json';
-  return config;
-});
+// api.interceptors.request.use((config) => {
+//   // Set headers or perform other request modifications
+//   config.headers['Content-Type'] = 'application/json';
+//   return config;
+// });
 
-// Response Interceptor
-api.interceptors.response.use(
-  (response) => {
-    // Handle successful responses
-    return response;
-  },
-  (error) => {
-    // Handle errors
-    throw error;
-  }
-);
+// // Response Interceptor
+// api.interceptors.response.use(
+//   (response) => {
+//     // Handle successful responses
+//     return response;
+//   },
+//   (error) => {
+//     // Handle errors
+//     throw error;
+//   }
+// );
 
 export default api;
